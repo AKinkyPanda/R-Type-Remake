f13bb68c065e1b2f89dacfb6649befedb98d2dd5
Sava Terzeesky <62663012+AKinkyPanda@users.noreply.github.com>
2025-10-16 10:19:55 +0300

    Init 3

---
+++ maps/level2-Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileSheet" tilewidth="64" tileheight="16" tilecount="268" columns="268">
+ <image source="tileSheet.png" width="17152" height="16"/>
+</tileset>
